2fba1c6bb0644e2e4de4a466b381f033878fec1b
John Emil Petersen III <johnemilpeterseniii@gmail.com>
2021-01-19 23:08:19 +0000

    increased hash size to accommodate enormous blacklist

--- add2Blacklist.sh
+++ add2Blacklist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ipset create blacklist hash:ip hashsize 4096
+ipset create blacklist hash:ip hashsize 256000
 for i in `cat ~/blackList/blacklist.txt`; do
 	ipset add blacklist $i
 done
